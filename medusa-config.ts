34719f704e452d4a5d962d26767c070b13448215
Prem tech <prem.upworktech@gmail.com>
2026-02-04 22:30:40 +0000

    UPDATED medusa config to align with railyway deployment

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,16 +1,19 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
-
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+const { defineConfig } = require("@medusajs/medusa")
 
 module.exports = defineConfig({
-  projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+    projectConfig: {
+        databaseUrl: process.env.DATABASE_URL,
+        http: {
+            storeCors: process.env.STORE_CORS || "*",
+            adminCors: process.env.ADMIN_CORS || "*",
+            authCors: process.env.AUTH_CORS || "*",
+        },
+        jwtSecret: process.env.JWT_SECRET || "supersecret",
+        cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+
+    // ✅ enable admin (served with backend)
+    admin: {
+        disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    },
 })
