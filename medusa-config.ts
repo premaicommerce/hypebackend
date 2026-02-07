c17294483e878cb24c068f26d3902d8ebceb93d0
Prem tech <prem.upworktech@gmail.com>
2026-02-07 19:48:06 +0000

    minio config added and caddy file prod config

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,15 +1,39 @@
 const { defineConfig } = require("@medusajs/medusa")
 
 module.exports = defineConfig({
-  projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS || "*",
-      adminCors: process.env.ADMIN_CORS || "*",
-      authCors: process.env.AUTH_CORS || "*",
+    projectConfig: {
+        databaseUrl: process.env.DATABASE_URL,
+        http: {
+            storeCors: process.env.STORE_CORS || "*",
+            adminCors: process.env.ADMIN_CORS || "*",
+            authCors: process.env.AUTH_CORS || "*",
+        },
+        jwtSecret: process.env.JWT_SECRET,
+        cookieSecret: process.env.COOKIE_SECRET,
     },
-    jwtSecret: process.env.JWT_SECRET,
-    cookieSecret: process.env.COOKIE_SECRET,
-  },
-  admin: { disable: false },
+    admin: { disable: false },
+     modules: [
+        {
+            resolve: "@medusajs/medusa/file",
+            options: {
+                providers: [
+                    {
+                        resolve: "@medusajs/medusa/file-s3",
+                        id: "s3",
+                        options: {
+                            file_url: process.env.S3_FILE_URL,
+                            access_key_id: process.env.S3_ACCESS_KEY_ID,
+                            secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+                            region: "us-east-1",
+                            bucket: process.env.S3_BUCKET,
+                            endpoint: process.env.S3_ENDPOINT,
+                            additional_client_config: {
+                                forcePathStyle: true, // This is the secret sauce for MinIO
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+    ],
 })
